b48f90d5bdab04888e6f3df6efd1081808294c9d
Bridger Herman <herma582@umn.edu>
2019-12-10 20:26:31 -0600

    Add halo movement to crowd

--- assets/user-scripts/lightManager.js
+++ assets/user-scripts/lightManager.js
@@ -44,8 +44,25 @@ export class LightManager extends WreScript {
 
     start() {
         // document.addEventListener('keydown', (evt) => this.keyHandler(evt));
+        this._time = 0.0;
+        this._frequency = 50.0;
+        this._amplitude = 0.01;
     }
 
     update() {
+        let dtSec = wre.time_dt().nanos / 10e9;
+        this._time += dtSec;
+
+        // Generate noise from -1.0 to 1.0
+        let sinusoid = Math.sin(this._frequency * this._time);
+
+        for (let i = 1; i < 27; i++) {
+            let noise = Math.random() * 2.0 - 1.0;
+            let yPerturb = (sinusoid + noise) * this._amplitude;
+
+            let light = wre.get_point_light(i);
+            light.position = light.position.add(new wre.Vec3(0.0, yPerturb, 0.0));
+            wre.set_point_light(i, light);
+        }
     }
 }
